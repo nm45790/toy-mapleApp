b2de2e5a32098da159c322e153f7c33f8c23fc49
nm45790 <nm45790@naver.com>
2022-10-24 18:19:48 +0900

    [변경] 전체 색상변경

--- client/components/NavBar.tsx
+++ client/components/NavBar.tsx
@@ -1,7 +1,7 @@
 export default function NavBar() {
   return (
     <>
-      <nav className="bg-color-1 pt-5 pb-5 px-16 text-color-2 text-2xl font-bold">
+      <nav className="bg-color-1 pt-5 pb-5 px-16 text-color-2 text-2xl font-bold fixed w-full shadow-md">
       <div className="flex justify-between ">
         <div >
           <p>Fun dev Blog</p>
