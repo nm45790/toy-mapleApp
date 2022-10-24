282cae9d5474d59807175fa6a757bf484a0ccf6d
nm45790 <nm45790@naver.com>
2022-10-24 11:23:51 +0900

    [setting] Recoil, sidebar atom

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,16 +2,19 @@ import "../styles/globals.css";
 import Layout from "../components/Layout";
 import Head from "next/head";
 import type { AppProps } from "next/app";
+import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+    <RecoilRoot>
       <Head>
         <title>toyMapleApp</title>
       </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      </RecoilRoot>
     </>
   );
 }
