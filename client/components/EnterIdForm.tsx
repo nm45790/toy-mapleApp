f98e0e967fb84b2f343ea83e0a4feeaf4b8f390d
nm45790 <nm45790@naver.com>
2022-11-09 18:47:02 +0900

    [폼작업] 폼에 입력한 데이터 추가

--- client/components/EnterIdForm.tsx
+++ client/components/EnterIdForm.tsx
@@ -12,10 +12,18 @@ type IDetailInfo = {
 export default function EnterIdForm() {
   const [charArr, setCharArr] = useState<IDetailInfo[]>([]);
   let arr: IDetailInfo[] = [];
-  const { register, handleSubmit, watch } = useForm<Inputs>();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     arr.push(data);
     console.log(arr);
+    setValue("name", "");
+    setCharArr((charArr) => [...charArr, ...arr]);
   };
   //   console.log(watch("name"));
   return (
@@ -26,8 +34,23 @@ export default function EnterIdForm() {
             <input
               className="h-12 w-64 border-solid border-2 border-color-4 rounded-lg"
               placeholder="Enter your name"
-              {...register("name")}
+              {...register("name", {
+                required: true,
+                minLength: {
+                  value: 2,
+                  message: "너무 짧습니다!",
+                },
+                maxLength: {
+                  value: 6,
+                  message: "너무 깁니다",
+                },
+              })}
             />
+            {errors.name?.type === "required" && (
+              <span>This field is required</span>
+            )}
+            {errors.name?.type === "minLength" && <span>It's too short</span>}
+            {errors.name?.type === "maxLength" && <span>It's too long</span>}
             <input
               className="w-16 ml-6 mr-6 bg-color-3 hover:bg-color-4 text-color-2 font-bold py-2 px-4 rounded-lg"
               value="&rarr;"
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
   return (
     <>
       <Seo title="Home" />
-      <p className="text-color-4">조회하고 싶은 캐릭터명을 기입 해주세요.</p>
+      <p>조회하고 싶은 캐릭터명을 기입 해주세요.</p>
 
       <EnterIdForm />
       <button className="mt-10 bg-color-3 hover:bg-color-4 text-color-2 font-bold py-2 px-4 rounded-full">
