ea116b665b4dc2d8c408786e6f275cd2a832bc10
mh <1545154@naver.com>
2022-11-11 01:22:32 +0900

    서버-클라이언트 통신 테스트 20221111

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -4,20 +4,45 @@ import { inputCharState } from "../state/inputCharState";
 import { useRecoilState } from "recoil";
 import InputIdForm from "../components/inputIdForm";
 import CharCards from "../components/charCards";
+import axios from 'axios';
 
 const Home: NextPage = () => {
   const [chars, setChars] = useRecoilState(inputCharState);
+
+  const searchApi = () => {
+        const url = "http://localhost:8080/api/searchInfo";
+        const id = '명회'
+        console.log(url);
+        console.log("id:" , id);
+        axios.get(url, {params:{ "id": id }})
+        .then(function(response) {
+            console.log(response.data);
+            console.log("성공");
+        })
+        .catch(function(error) {
+            console.log(error);
+            console.log("실패");
+        })
+  }
+
   return (
     <>
       <Seo title="Home" />
       <p>조회하고 싶은 캐릭터명을 기입 해주세요.</p>
       <InputIdForm />
       <button
-        onClick={() => setChars([])}
-        className="mt-10 bg-color-3 hover:bg-color-4 text-color-2 font-bold py-2 px-4 rounded-full"
-      >
-        초기화
+            onClick={() => setChars([])}
+            className="mt-10 bg-color-3 hover:bg-color-4 text-color-2 font-bold py-2 px-4 rounded-full">
+            초기화
       </button>
+
+      <button
+            onClick={searchApi}
+            className="mt-10 bg-color-3 hover:bg-color-4 text-color-2 font-bold py-2 px-4 rounded-full">
+            조회
+      </button>
+
+
       <CharCards/>
     </>
   );
--- src/main/java/toy/mapleStory/controller/jsonController.java
+++ src/main/java/toy/mapleStory/controller/jsonController.java
@@ -1,16 +1,24 @@
 package toy.mapleStory.controller;
 
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import toy.mapleStory.service.searchInfoService;
+import toy.mapleStory.vo.searchVO;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins="*", allowedHeaders="*") /* CORS 어노테이션 */
 @RestController
 public class jsonController {
+
+    @Autowired
+    private searchInfoService searchService;
 
     @GetMapping(value="/callApi")
     public String frontCallApi(){
@@ -35,5 +43,36 @@ public class jsonController {
         return json.toString();
     }
 
+    @GetMapping("/api/searchInfo")
+    public String searchInfo(@RequestParam(value="id", required=false) String id) throws InterruptedException {
+
+        System.out.println("요청 도착");
+        System.out.println(id);
+
+        // 검색 캐릭터 이름, 직업, 레벨, 경험치, 인기도, 길드 정보
+        List<searchVO> characterInfo = searchService.infoList(id);
+
+        // 검색 캐릭터 메소, 인벤토리 / 창고 링크
+        searchService.subInfoList(characterInfo);
+
+        // 검색 캐릭터 메소 합
+        Long totalMoney = searchService.calcTotalMoney(characterInfo);
+//
+//        검색 캐릭터 인벤토리
+        List<Map> itemMapList = searchService.itemInfoList(characterInfo);
+//
+//      검색 캐릭터 창고 메소
+        String storageMoney = searchService.getStorageMoney(characterInfo);
+
+        JSONObject json = new JSONObject();
+        json.put("characterInfo", characterInfo);
+        json.put("characterTotalMoney", totalMoney);
+        json.put("itemMapList", itemMapList);
+        json.put("storageMoney", storageMoney);
+
+        System.out.println(json);
+
+        return json.toString();
+    }
 }
 
