34a80ecb81758cffcf3ff2281cb1377a7ec4b185
nm45790 <nm45790@naver.com>
2022-11-17 14:37:48 +0900

    [테스트] 테스트값 변경, 리소스추가

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -33,8 +33,7 @@ const Home: NextPage = () => {
       <p>조회하고 싶은 캐릭터명을 기입 해주세요.</p>
       <InputIdForm />
       <button
-            // onClick={() => setChars([])}
-            onClick={()=>console.log(chars)}
+            onClick={() => setChars([])}
             className="mt-10 bg-color-3 hover:bg-color-4 text-color-2 font-bold py-2 px-4 rounded-full">
             초기화
       </button>
