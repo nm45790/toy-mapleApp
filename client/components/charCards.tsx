83680bf74bd1fe3f110e660b639cda2ba43dee24
nm45790 <nm45790@naver.com>
2022-11-24 17:42:47 +0900

    [테스트] 인벤토리 모달창 업데이트

--- client/components/charCards.tsx
+++ client/components/charCards.tsx
@@ -4,37 +4,48 @@ import Image from "next/image";
 import Manikin from "../../resource/마네킹.png";
 import axios from "axios";
 import React from "react";
-import Mordal from "./mordal";
+import { MdInventory } from "react-icons/Md";
 
 interface CharsInterface {
-  lv : number;
-  job : string;
+  lv: number;
+  job: string;
 }
 
-type JonType =  {
-  job : string;
-}
+type JonType = {
+  job: string;
+};
 
 export default function CharCards() {
   const [chars, setChars] = useRecoilState(inputCharState);
   const updatedChars = [...chars];
+  const [showModal, setShowModal] = React.useState(false);
 
   const [datas, setDatas] = React.useState([]);
   const [guildData, setGuildData] = React.useState<string[]>([]);
   const [mesoData, setMesoData] = React.useState<number[]>([]);
   const [lvData, setLvData] = React.useState<number[]>([]);
   const [jobData, setJobData] = React.useState<string[]>([]);
+  const [equipData, setEquipData] = React.useState<string[]>([]);
+  const [useData, setUseData] = React.useState<string[]>([]);
+  const [etcData, setEtcData] = React.useState<string[]>([]);
+  const [setupData, setSetupData] = React.useState<string[]>([]);
+  const [cashData, setCashData] = React.useState<string[]>([]);
 
   const searchApi = () => {
     axios
       .all(chars.map((v) => axios.get(`${v.name}.json`)))
       //서버에 요청할때 : axios.get(`localhost8080/api/blabla/${v.name}`)
-      .then(response => {
+      .then((response) => {
         console.log("성공");
-        setMesoData(response.map(v=>v.data.mapleMoney))
-        setGuildData(response.map(v=>v.data.guild))
-        setJobData(response.map(v=>v.data.job))
-        setLvData(response.map(v=>v.data.lv))
+        setMesoData(response.map((v) => v.data.mapleMoney));
+        setGuildData(response.map((v) => v.data.guild));
+        setJobData(response.map((v) => v.data.job));
+        setLvData(response.map((v) => v.data.lv));
+        setEquipData(response.map((v) => v.data.equip));
+        setUseData(response.map((v) => v.data.use));
+        setEtcData(response.map((v) => v.data.etc));
+        setSetupData(response.map((v) => v.data.setup));
+        setCashData(response.map((v) => v.data.cash));
       })
       .catch((error) => {
         console.log(error);
@@ -49,7 +60,6 @@ export default function CharCards() {
       >
         조회
       </button>
-      <Mordal />
       <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-4">
         {chars &&
           chars.map((v, i) => (
@@ -63,17 +73,93 @@ export default function CharCards() {
               >
                 삭제
               </button>
-              {/* <button
-                onClick={() => {
-                  console.log(cashData);
-                }}
-                className="mt-10 bg-color-3 hover:bg-color-4 text-color-2 font-bold py-2 px-4 rounded-full"
-              >
-                조회
-              </button> */}
               <div className="max-w-sm rounded overflow-hidden shadow-lg bg-color-3 mt-2 ">
                 <div className="px-6 py-4">
-                  <div className="font-bold text-xl mb-2">{v.name}</div>
+                  <div className="font-bold text-xl mb-2 flex justify-between">
+                    <p>{v.name}</p>
+                    <button onClick={() => setShowModal(true)}>
+                      <MdInventory />
+                    </button>
+                    {showModal ? (
+                      <>
+                        <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                          <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                            {/*content*/}
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                              {/*header*/}
+                              <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                <h3 className="text-3xl font-semibold">
+                                  인벤토리
+                                </h3>
+                                <button
+                                  className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                  onClick={() => setShowModal(false)}
+                                >
+                                  <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                    ×
+                                  </span>
+                                </button>
+                              </div>
+                              {/*body*/}
+                              <div className="relative p-6 flex-auto grid lg:grid-cols-12 md:grid-cols-8 grid-cols-4 gap-4 overflow-auto max-h-[80vh]">
+                                {Object.keys(equipData[i]).map((v) => (
+                                  <div
+                                    id={v}
+                                    className="shadow-lg p-6 bg-red-50"
+                                  >
+                                    <p className="text-xs">{v}</p>
+                                  </div>
+                                ))}
+                                {Object.keys(useData[i]).map((v) => (
+                                  <div
+                                    id={v}
+                                    className="shadow-lg p-6 bg-red-100"
+                                  >
+                                    <p className="text-xs">{v}</p>
+                                  </div>
+                                ))}
+                                {Object.keys(etcData[i]).map((v) => (
+                                  <div
+                                    id={v}
+                                    className="shadow-lg p-6 bg-red-200"
+                                  >
+                                    <p className="text-xs">{v}</p>
+                                  </div>
+                                ))}
+                                {Object.keys(setupData[i]).map((v) => (
+                                  <div
+                                    id={v}
+                                    className="shadow-lg p-6 bg-red-300"
+                                  >
+                                    <p className="text-xs">{v}</p>
+                                  </div>
+                                ))}
+                                {Object.keys(cashData[i]).map((v) => (
+                                  <div
+                                    id={v}
+                                    className="shadow-lg p-6 bg-red-400"
+                                  >
+                                    <p className="text-xs">{v}</p>
+                                  </div>
+                                ))}
+                              </div>
+                              {/*footer*/}
+                              <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                <button
+                                  className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                  type="button"
+                                  onClick={() => setShowModal(false)}
+                                >
+                                  Close
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                      </>
+                    ) : null}
+                  </div>
                   <p className="text-gray-700 text-base"> </p>
                   <Image src={Manikin} />
                   {/* 이미지 데이터 바꿔야함 */}
@@ -82,17 +168,6 @@ export default function CharCards() {
                   <p>길드 : {guildData[i]} </p>
                   <p>보유메소 : {mesoData[i]}</p>
                 </div>
-                {/* <div className="px-6 pt-4 pb-2">
-                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                    #모험가
-                  </span>
-                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                    #본캐급
-                  </span>
-                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                    #무릉1짱
-                  </span>
-                </div> */}
               </div>
             </div>
           ))}
